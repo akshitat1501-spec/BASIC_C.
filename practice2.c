c2dfaac3179e7b398f2f8b5f51551b0b7b284f39
Akshita Thakur <akshita.t1501@gmail.com>
2026-01-28 21:18:53 +0530

    Add files via upload
    #learning basics of c

---
+++ practice2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int x = 7;
+	int y = 5;
+	printf("%d\t",x++);
+	printf("%d\t",x);
+	printf("%d\t",++x);
+	printf("%d\n",x);
+	printf("%d\t",++y);
+	printf("%d\t",y);
+	
+}

b25337939c1995b1b900546a2780365a54aba49f
Akshita Thakur <akshita.t1501@gmail.com>
2026-01-28 21:19:48 +0530

    Add files via upload
    practicing c language   #understanding basics

---
+++ practice3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/*int main()
+{
+	char a,b;
+	scanf("%c %c",&a,&b);
+	
+}
+int main()
+{
+	int age;
+	printf("enter your age");
+	scanf("%d",&age);
+	age>=18?printf("Allowed to vote"):printf("not allowed to vote");
+	return 0;
+}
+int main()
+{
+	int num;
+	printf("enter a number: ");
+	scanf("%d",&num);
+	(num % 2 == 0) ? printf("even number") : printf("odd number");
+	return 0;
+}*/
+int main()
+{
+	int a = 10, b = 5, c = 5;
+	int d;
+	d = b + c == a;
+	printf("%d",d);
+	return 0;
+	
+}
+
